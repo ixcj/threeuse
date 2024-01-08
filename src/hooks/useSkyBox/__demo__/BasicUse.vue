a862edf82f4fed6fe068bed5e1fe23a7d8f5f2c8
Xiecj <i@xcj.im>
2024-01-08 13:56:59 +0800

    feat:  修改demo

--- src/hooks/useSkyBox/__demo__/BasicUse.vue
+++ src/hooks/useSkyBox/__demo__/BasicUse.vue
@@ -1,45 +1,11 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { createApp, useRenderClock, useSkyBox } from 'threeuse'
-import {
-  BoxGeometry,
-  MeshBasicMaterial,
-  Mesh,
-  Color,
-  PlaneGeometry,
-} from 'three'
+import { createApp, useSkyBox } from 'threeuse'
 
 const app = createApp({ cameraPosition: [0.74, 4.53, -26.56] })
 const scene = app.getScene()
 
-const geometry = new BoxGeometry(1, 1, 1)
-const mats = []
-for(let i = 0; i < 6; i++){
-  let material = new MeshBasicMaterial({ color: new Color(Math.random() * 0xffffff)})
-  mats.push(material)
-}
-
-const cube = new Mesh(geometry, mats)
-cube.name = 'cube'
-cube.position.setY(1)
-scene.add(cube)
-
-const floorGeo = new PlaneGeometry(2000, 2000, 1, 1)
-const floorMaterial = new MeshBasicMaterial({ color: '#aaaaaa' })
-const floor = new Mesh(floorGeo, floorMaterial)
-floor.rotateX(Math.PI / -2)
-scene.add(floor)
-
-app.getRenderer().shadowMap.enabled = true
-const { value } = useSkyBox(scene, {
-  castShadowList: ['cube']
-})
-
-useRenderClock((d) => {
-  const rotation = d / 1000
-  cube.rotation.x += rotation
-  cube.rotation.y += rotation
-})
+const { value } = useSkyBox(scene)
 
 onMounted(() => app.mount('#app-container'))
 </script>
